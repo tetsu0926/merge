06905bbff5fb35d5c98fe09803ec4791505232fa
totoro <tetsu0926@gmail.com>
2023-08-07 23:00:23 +0900

    인텔리제이

--- Test/src/co/kr/test/Test.java
+++ Test/src/co/kr/test/Test.java
@@ -2,7 +2,7 @@ package co.kr.test;
 
 public class Test {
 	
-	
+	public static final String CODE_CONSTRANTS = "상수";
 	
 	public static void main(String[] args) {
 		
@@ -17,6 +17,7 @@ public class Test {
 	public void test(){
 		System.out.println("8/7 - 이클립스에서 작성 ");
 		System.out.println("8/7 - 인텔리제이 작성");
+		System.out.println("오우...새로운 테마...아니 새로운 UI 적응 안된다 ㅎㅎ");
 	}
 
 }
