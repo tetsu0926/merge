9eba29ec58922e89fa714f340a7050b68dd2399f
totoro <tetsu0926@gmail.com>
2023-09-23 14:10:33 +0900

    인텔리제이에서 커밋

--- Test/src/co/kr/test/Test.java
+++ Test/src/co/kr/test/Test.java
@@ -6,11 +6,9 @@ public class Test {
 	
 	public static void main(String[] args) {
 
-		
 
-		System.out.println("+++++++++++++++++++++");
-		System.out.println("====================");
-        System.out.println("+++++++++++++++++++++");
+
+
 
 		int turmCnt = 50;
 		int dayDiv = 9;
